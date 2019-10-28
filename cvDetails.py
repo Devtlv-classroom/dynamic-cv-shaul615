5312ac9ab0acac85fd3dde6027ab9bbad57681fb
shaul615 <55354835+shaul615@users.noreply.github.com>
2019-10-28 17:58:47 +0200

    Add files via upload

---
+++ cvDetails.py
@@ -0,0 +1,23 @@
+full_name = "Shaul Leor-Lever"
+skills = "python, excel, html"
+expierence = "Uni-lever Dis-Chem"
+mobile_number = "055-948-3038"
+address_line1 = "35 Carevelot"
+address_line2 = "Vered Drive"
+address_line3 = "Mitzpe Netofa"
+city= " Galilee"
+country = " Israel"
+
+
+print('<h1>This is a cv for: {}</h1>'.format(full_name))
+print('Address: <br>{}<br>{}<br>{}<br>{}<br>{}'.format(address_line1,address_line2,address_line3,city,country))
+print('<hr><p>Skills:{}</p>'.format(skills))
+print('<hr><p>Expierence :{}</p>'.format(expierence))
+
+
+
+
+
+
+
+#character_type =
